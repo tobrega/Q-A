462f553333baf4a8d3bf3191519a88ccfc202b34
Gabe Acevedo <gea2111@columbia.edu>
2021-05-13 09:19:12 -0700

    added indexed for questions table

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,38 @@
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS answers;
-DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS answers_photos;
 
 CREATE TABLE questions (
-  id INT PRIMARY KEY,
+  id INT,
   product_id INT,
   body VARCHAR(255) NOT NULL,
   date_written DATE,
   asker_name VARCHAR(55) NOT NULL,
   asker_email VARCHAR(55),
   reported BOOLEAN NOT NULL,
-  helpfulness INT
+  helpfulness INT,
+  PRIMARY KEY (id)
 );
 
+-- DROP INDEX IF EXISTS questions_id_index;
+DROP INDEX IF EXISTS questions_product_id_index;
+
+-- CREATE INDEX questions_id_index ON questions (id);
+CREATE INDEX questions_product_id_index ON questions (product_id);
+
 CREATE TABLE answers (
-  id INT PRIMARY KEY,
-  question_id INT,
+  id INT,
+  question_id INT ,
   body VARCHAR(255) NOT NULL,
   date_written DATE,
   answerer_name VARCHAR(55) NOT NULL,
   answerer_email VARCHAR(55),
   reported BOOLEAN NOT NULL,
-  helpfulness INT
+  helpfulness INT,
+  PRIMARY KEY (id)
+  -- CONSTRAINT fk_question
+  --   FOREIGN KEY (question_id)
+  --     REFERENCES questions
 );
 
 CREATE TABLE answers_photos (
